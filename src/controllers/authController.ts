b782984254d7b84f3eb2e8af38ac2ad361e80ced
Faizan <fkhan698@hotmail.com>
2023-02-27 18:11:38 -0600

    Add Goal CRUD

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,9 @@
-import { Request, Response, NextFunction } from 'express'
-import jwt from 'jsonwebtoken'
-import validateUser from '../utils/userValidation'
-import { IUser } from '../models/IUser'
-import isUserAuthentic from '../services/authService'
-import config from '../config'
+import { Request, Response, NextFunction } from "express"
+import jwt from "jsonwebtoken"
+import validateUser from "../utils/userValidation"
+import { IUser } from "../models/IUser"
+import isUserAuthentic from "../services/authService"
+import config from "../config"
 
 export const login = async (req: Request, res: Response) => {
   const { body } = req
@@ -18,26 +18,29 @@ export const login = async (req: Request, res: Response) => {
 
   const userAuthenticated = await isUserAuthentic(user)
   if (!userAuthenticated) {
-    res.send('User or password are incorrect')
+    res.send("User or password are incorrect")
     return
   }
 
   const { privateKey } = config.jwt
-  const token = jwt.sign(
-    { user: user.email },
-    privateKey as jwt.Secret,
-    { algorithm: 'RS256', expiresIn: '1h' },
-  )
+  const token = jwt.sign({ user: user.email }, privateKey as jwt.Secret, {
+    algorithm: "RS256",
+    expiresIn: "1h",
+  })
 
-  res.cookie(
-    'jwt_token',
-    token,
-    { httpOnly: true, secure: false, sameSite: true },
-  )
-  res.send('Logged in')
+  res.cookie("jwt_token", token, {
+    httpOnly: true,
+    secure: false,
+    sameSite: true,
+  })
+  res.send("Logged in")
 }
 
-export const logout = async (req: Request, res: Response, next: NextFunction) => {
+export const logout = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   next()
-  res.send('Logout')
+  res.send("Logout")
 }
