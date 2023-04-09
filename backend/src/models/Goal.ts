af9e57711c297d18945bd8af664c065328f1b600
Faizan <fkhan698@hotmail.com>
2023-04-09 14:10:24 -0500

    Add react frontend

--- backend/src/models/Goal.ts
+++ backend/src/models/Goal.ts
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose'
 import { IGoal } from './IGoal'
+import slugify from 'slugify'
 
 const goalSchema = new Schema<IGoal>({
   title: { type: String, required: true },
@@ -7,7 +8,14 @@ const goalSchema = new Schema<IGoal>({
   deadline: { type: Date, required: true },
   recipientEmail: { type: String, required: true },
   completed: { type: Boolean, default: false },
+  slug: {type: String, unique: true},
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+})
+
+goalSchema.pre('validate', function(next) {
+  if(this.title) {
+    this.slug = slugify(this.title, {lower: true, strict: true})
+  }
 })
 
 export default mongoose.model('Goal', goalSchema, 'goals')
--- backend/src/models/IGoal.ts
+++ backend/src/models/IGoal.ts
@@ -2,10 +2,11 @@ import { Types } from 'mongoose'
 
 export interface IGoal {
   id?: Types.ObjectId
-  title: String
+  title: string
   description: String
   deadline: Date
   recipientEmail: String
   completed: Boolean
+  slug: string
   user: Types.ObjectId
 }
